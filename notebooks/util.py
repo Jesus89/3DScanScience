730557a9d0774cb81d2a54c50e6fec0bcaaa3f7f
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-04-22 14:26:56 +0200

    Updated point cloud in Laser calibration

--- notebooks/util.py
+++ notebooks/util.py
@@ -1,51 +1,60 @@
 import cv2
 import math
 import pylab
+import struct
 import datetime
 import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 _begin = 0
 total_time = datetime.timedelta()
+
 
 # Time measurement functions
 
 def begin():
     global _begin
     _begin = datetime.datetime.now()
-    
+
+
 def end():
     global _begin, total_time
     end = datetime.datetime.now() - _begin
     total_time += end
     print('Time: %s' % end)
 
+
 def total():
     global total_time
     print('Total time: %s' % total_time)
+
 
 # Plot image functions
 
 def plot_image(image):
     if len(image.shape) == 2:
-        image = cv2.merge((image,image,image))
-    f, axarr = plt.subplots(1, 1, figsize=(10,15))
+        image = cv2.merge((image, image, image))
+    f, axarr = plt.subplots(1, 1, figsize=(10, 15))
     axarr.axis('off')
     axarr.imshow(image)
     axarr.plot()
-    
+
+
 def plot_images(images):
-    f, axarr = plt.subplots(1, len(images), figsize=(15,15))
+    f, axarr = plt.subplots(1, len(images), figsize=(15, 15))
     for i in range(len(images)):
         if len(images[i].shape) == 2:
-            image = cv2.merge((images[i],images[i],images[i]))
+            image = cv2.merge((images[i], images[i], images[i]))
         else:
             image = images[i]
         axarr[i].axis('off')
         axarr[i].imshow(image)
         axarr[i].plot()
-        
+
+
 # Load image function
 
 def load_image(path):
@@ -53,9 +62,11 @@ def load_image(path):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # Pattern parameters
+
 rows = 6
 columns = 11
 square_width = 13
+
 
 # Pattern detection functions
 
@@ -71,8 +82,9 @@ def pattern_detection(image):
 
     # Find corners with subpixel accuracy
     cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-    
+
     return corners, ret
+
 
 def draw_pattern(image, corners, ret):
     # Draw corners into image
@@ -81,3 +93,87 @@ def draw_pattern(image, corners, ret):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     return image
+
+
+# Read PLY functions
+
+class Mesh(object):
+    def __init__(self):
+        self.vertexes = None
+        self.colors = None
+        self.normal = None
+        self.vertex_count = 0
+
+
+def _load_binary(mesh, stream, dtype, count):
+    data = np.fromfile(stream, dtype=dtype, count=count)
+
+    fields = dtype.fields
+    mesh.vertex_count = count
+
+    if 'v' in fields:
+        mesh.vertexes = data['v']
+    else:
+        mesh.vertexes = np.zeros((count, 3))
+
+    if 'n' in fields:
+        mesh.normal = data['n']
+    else:
+        mesh.normal = np.zeros((count, 3))
+
+    if 'c' in fields:
+        mesh.colors = data['c']
+    else:
+        mesh.colors = 255 * np.ones((count, 3))
+
+
+def load_ply(filename):
+    m = Mesh()
+    with open(filename, "rb") as f:
+        dtype = []
+        count = 0
+        format = None
+        line = None
+        header = ''
+
+        while line != 'end_header\n' and line != '':
+            line = f.readline()
+            header += line
+        # Discart faces
+        header = header.split('element face ')[0].split('\n')
+
+        if header[0] == 'ply':
+
+            for line in header:
+                if 'format ' in line:
+                    format = line.split(' ')[1]
+                    break
+
+            if format is not None:
+                if format == 'ascii':
+                    fm = ''
+                elif format == 'binary_big_endian':
+                    fm = '>'
+                elif format == 'binary_little_endian':
+                    fm = '<'
+
+            df = {'float': fm + 'f', 'uchar': fm + 'B'}
+            dt = {'x': 'v', 'nx': 'n', 'red': 'c', 'alpha': 'a'}
+            ds = {'x': 3, 'nx': 3, 'red': 3, 'alpha': 1}
+
+            for line in header:
+                if 'element vertex ' in line:
+                    count = int(line.split('element vertex ')[1])
+                elif 'property ' in line:
+                    props = line.split(' ')
+                    if props[2] in dt.keys():
+                        dtype = dtype + [(dt[props[2]], df[props[1]], (ds[props[2]],))]
+
+            dtype = np.dtype(dtype)
+
+            if format is not None:
+                if format == 'binary_big_endian' or format == 'binary_little_endian':
+                    _load_binary(m, f, dtype, count)
+            return m
+        else:
+            return None
